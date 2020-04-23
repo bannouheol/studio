3b4c7f223c14f4a71f3eaa804cee1fd3ed90886b
Joris <joris@saidani.org>
2020-04-24 01:57:09 +0200

    remove required previousPath

--- schemas/document/blog/post.js
+++ schemas/document/blog/post.js
@@ -1,136 +1,134 @@
-import { MdLocalPostOffice } from "react-icons/md";
-import { parseISO, format } from "date-fns";
+import { MdLocalPostOffice } from 'react-icons/md'
+import { parseISO, format } from 'date-fns'
 
 export default {
-  name: "blogPost",
-  title: "Article de presse / Blog Post",
-  type: "document",
+  name: 'blogPost',
+  title: 'Article de presse / Blog Post',
+  type: 'document',
   icon: MdLocalPostOffice,
   fields: [
     {
-      title: "Language",
-      name: "language",
-      type: "array",
-      of: [{ type: "string" }],
+      title: 'Language',
+      name: 'language',
+      type: 'array',
+      of: [{ type: 'string' }],
       options: {
         list: [
-          { title: "Breton", value: "br" },
-          { title: "Français", value: "fr" },
+          { title: 'Breton', value: 'br' },
+          { title: 'Français', value: 'fr' },
         ],
       },
       validation: (Rule) => Rule.required(),
     },
     {
-      name: "publishedAt",
-      title: "Date de publication",
-      type: "date",
+      name: 'publishedAt',
+      title: 'Date de publication',
+      type: 'date',
       validation: (Rule) => Rule.required(),
     },
     {
-      name: "title",
-      title: "Titre du post",
-      type: "localeString",
+      name: 'title',
+      title: 'Titre du post',
+      type: 'localeString',
     },
     {
-      name: "slug",
-      title: "Slug",
-      type: "localeSlug",
+      name: 'slug',
+      title: 'Slug',
+      type: 'localeSlug',
     },
     {
-      name: "categories",
-      title: "Catégories",
-      type: "array",
+      name: 'categories',
+      title: 'Catégories',
+      type: 'array',
       of: [
         {
-          type: "reference",
-          to: { type: "blogCategory" },
+          type: 'reference',
+          to: { type: 'blogCategory' },
         },
       ],
     },
     {
-      name: "image",
-      type: "image",
-      title: "Image",
+      name: 'image',
+      type: 'image',
+      title: 'Image',
     },
     {
-      title: "Profile(s) relié(s)",
-      name: "featuredProfiles",
-      type: "array",
+      title: 'Profile(s) relié(s)',
+      name: 'featuredProfiles',
+      type: 'array',
       of: [
         {
-          type: "reference",
-          to: { type: "profile" },
+          type: 'reference',
+          to: { type: 'profile' },
         },
       ],
     },
     {
-      title: "Audio",
-      name: "audio",
-      type: "file",
+      title: 'Audio',
+      name: 'audio',
+      type: 'file',
     },
     {
-      title: "Vidéo YouTube",
-      name: "video",
-      type: "youtube",
+      title: 'Vidéo YouTube',
+      name: 'video',
+      type: 'youtube',
     },
     {
-      name: "excerpt",
-      title: "Extrait",
-      type: "localeText",
+      name: 'excerpt',
+      title: 'Extrait',
+      type: 'localeText',
     },
     {
-      name: "body",
-      title: "Contenu",
-      type: "localeBlockContent",
+      name: 'body',
+      title: 'Contenu',
+      type: 'localeBlockContent',
     },
     {
-      name: "author",
-      title: "Auteur (journal, radio, ...)",
-      type: "reference",
-      to: { type: "blogAuthor" },
+      name: 'author',
+      title: 'Auteur (journal, radio, ...)',
+      type: 'reference',
+      to: { type: 'blogAuthor' },
     },
     {
-      name: "products",
-      title: "Relier à des produits",
-      type: "array",
+      name: 'products',
+      title: 'Relier à des produits',
+      type: 'array',
       of: [
         {
-          type: "reference",
-          to: [{ type: "product" }],
+          type: 'reference',
+          to: [{ type: 'product' }],
         },
       ],
     },
     {
-      name: "previousPath",
-      title: "Adresse ancien site",
-      type: "string",
-      validation: (Rule) => Rule.required(),
+      name: 'previousPath',
+      title: 'Adresse ancien site',
+      type: 'string',
     },
   ],
   preview: {
     select: {
-      language: "language",
-      titleBr: "title.br",
-      titleFr: "title.fr",
-      media: "image",
-      date: "publishedAt",
+      language: 'language',
+      titleBr: 'title.br',
+      titleFr: 'title.fr',
+      media: 'image',
+      date: 'publishedAt',
     },
     prepare(selection) {
-      const { language, titleFr, titleBr, media, date } = selection;
-      const formattedDate =
-        typeof date !== "undefined" ? format(parseISO(date), "dd/MM/yyyy") : "";
+      const { language, titleFr, titleBr, media, date } = selection
+      const formattedDate = typeof date !== 'undefined' ? format(parseISO(date), 'dd/MM/yyyy') : ''
       return {
-        title: language[0] === "br" ? titleBr : titleFr,
+        title: language[0] === 'br' ? titleBr : titleFr,
         subtitle: formattedDate,
         media,
-      };
+      }
     },
   },
   orderings: [
     {
-      title: "Date de publication, nouveaux",
-      name: "publishedAtDesc",
-      by: [{ field: "publishedAt", direction: "desc" }],
+      title: 'Date de publication, nouveaux',
+      name: 'publishedAtDesc',
+      by: [{ field: 'publishedAt', direction: 'desc' }],
     },
   ],
-};
+}
