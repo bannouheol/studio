04453a87b9c479aafe0612d72aab04e06fe43524
Joris <joris@saidani.org>
2020-08-11 16:25:02 +0200

    up

---
+++ schemas/object/imageWithCaption.js
@@ -0,0 +1,25 @@
+export default {
+  title: 'Image with caption',
+  name: 'imageWithCaption',
+  type: 'image',
+  options: {
+    hotspot: true, // <-- Defaults to false
+  },
+  fields: [
+    {
+      name: 'caption',
+      type: 'localeString',
+      title: 'Caption',
+      options: {
+        isHighlighted: true, // <-- make this field easily accessible
+      },
+      //validation: (Rule) => Rule.required(),
+    },
+    {
+      // Editing this field will be hidden behind an "Edit"-button
+      name: 'attribution',
+      type: 'localeString',
+      title: 'Attribution',
+    },
+  ],
+}
